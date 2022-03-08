756799fba269a3e704dab603a548d73b6ecc9dad
Lucas Sales <lucas.gol@uol.com.br>
2022-03-07 22:47:50 -0300

    Correcao de Activity x Fragment

--- app/src/main/java/com/example/clima/MainActivity.kt
+++ app/src/main/java/com/example/clima/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.clima
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentContainer: FragmentContainerView by lazy {findViewById(R.id.fragment_container)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, LoginFragment())
+            .commit()
     }
 }
