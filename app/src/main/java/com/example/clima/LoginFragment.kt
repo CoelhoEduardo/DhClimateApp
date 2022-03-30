c8013b5e51697013abb0291fd011e0b223ca576f
Lucas Sales <lucas.gol@uol.com.br>
2022-03-29 23:03:46 -0300

    Criado base de login google

--- app/src/main/java/com/example/clima/LoginFragment.kt
+++ app/src/main/java/com/example/clima/LoginFragment.kt
@@ -10,8 +10,12 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.example.clima.Utils.GoogleLogInActivityContract
 import com.example.clima.Utils.checkEmail
 import com.example.clima.viewmodel.ForgotPasswordFragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.textfield.TextInputEditText
 import java.lang.Exception
@@ -19,17 +23,38 @@ import java.lang.Exception
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    private val googleSignInRequest = registerForActivityResult(
+        GoogleLogInActivityContract(),
+        ::onGoogleSignInResult
+    )
+
+    private val googleSignInOptions : GoogleSignInOptions
+        get() = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestProfile()
+            .build()
+
+
+    //lateinit var googleSignInClient : GoogleSignInClient
+
+
+    /*private val google: Button?
+        get() = view?.findViewById(R.id.google_button)
+*/
+
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val login = view.findViewById<Button>(R.id.login_button)
         val newAccount = view.findViewById<Button>(R.id.newaccount_button)
-        val google = view.findViewById<ImageView>(R.id.google_button)
         val facebook = view.findViewById<ImageView>(R.id.facebook_button)
         val loginEdit = view.findViewById<TextInputEditText>(R.id.login_edit_text)
         val passwordText = view.findViewById<TextInputEditText>(R.id.password_edit_text)
         val forgotPassword = view.findViewById<Button>(R.id.forgotpass_button)
+         val google = view.findViewById<ImageView>(R.id.google_button)
+
+
 
         val dialog = ForgotPasswordFragment()
 
@@ -50,8 +75,12 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             sendToNewAccount()
         }
 
-        google.setOnClickListener {
-            sendToGoogle()
+        //googleSignInClient = GoogleSignIn.getClient(view.context, googleSignInOptions)
+
+        google?.setOnClickListener {
+            //sendToGoogle()
+            googleSignInRequest.launch(googleSignInOptions)
+
         }
 
         facebook.setOnClickListener {
@@ -95,6 +124,13 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     private fun sendToHome() {
 
         findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+    }
+
+    private fun onGoogleSignInResult(result: GoogleLogInActivityContract.Result?) {
+        if(result is GoogleLogInActivityContract.Result.Success){
+            val token = result.googleSignInAccount.idToken
+            Toast.makeText(requireContext(), "Meu token do google e -> $token", Toast.LENGTH_LONG).show()
+        }
     }
 
 
