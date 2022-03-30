493242953e81af23e44501632452864aef9ffc10
Lucas Sales <lucas.gol@uol.com.br>
2022-03-29 21:19:37 -0300

    Criado arquivo de assinatura

--- app/src/main/java/com/example/clima/Model/Maps/Search.kt
+++ app/src/main/java/com/example/clima/Model/Maps/Search.kt
@@ -1,7 +1,9 @@
 package com.example.clima.Model.Maps
 
+import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+
+import kotlinx.parcelize.Parcelize
 import java.util.*
 
 @Parcelize
@@ -9,5 +11,13 @@ class Search(
     val image: String,
     val local: String,
     val data: String
-) : Parcelable
+) : Parcelable {
+    override fun describeContents(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun writeToParcel(p0: Parcel?, p1: Int) {
+        TODO("Not yet implemented")
+    }
+}
 
