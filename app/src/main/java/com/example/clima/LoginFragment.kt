5f7d4d318cb637f39eeba59e31da9a181317a8e4
Lucas Sales <lucas.gol@uol.com.br>
2022-03-31 19:27:22 -0300

    Google login ok

--- app/src/main/java/com/example/clima/LoginFragment.kt
+++ app/src/main/java/com/example/clima/LoginFragment.kt
@@ -30,6 +30,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private val googleSignInOptions : GoogleSignInOptions
         get() = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.google_client_id))
             .requestEmail()
             .requestProfile()
             .build()
@@ -46,16 +47,8 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         get() = view?.findViewById(R.id.google_button)
 */
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-
 
         val login = view.findViewById<Button>(R.id.login_button)
         val newAccount = view.findViewById<Button>(R.id.newaccount_button)
@@ -64,8 +57,6 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         val passwordText = view.findViewById<TextInputEditText>(R.id.password_edit_text)
         val forgotPassword = view.findViewById<Button>(R.id.forgotpass_button)
         val google = view.findViewById<ImageView>(R.id.google_button)
-
-
 
         val dialog = ForgotPasswordFragment()
 
@@ -91,7 +82,6 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         google?.setOnClickListener {
             //sendToGoogle()
             googleSignInRequest.launch(googleSignInOptions)
-
         }
 
         facebook.setOnClickListener {
@@ -130,23 +120,6 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         }
         )
     }
-/*
-    private fun loginFacebook() {
-        loginManager.logInWithReadPermissions(this, arrayListOf("public profile"))
-        loginManager.registerCallback(callbackManager,object : FacebookCallback<LoginResult> {
-            override fun onSuccess(result: LoginResult) {
-                Toast.makeText(requireContext(),"Esse e o nosso token -> ${result.accessToken.token}", Toast.LENGTH_LONG).show()
-            }
-            override fun onCancel() {
-                Toast.makeText(requireContext(),"Cancelou", Toast.LENGTH_LONG).show()
-            }
-            override fun onError(error: FacebookException?) {
-                Toast.makeText(requireContext(),"Erro!", Toast.LENGTH_LONG).show()
-                println(error)
-            }
-        }
-        )
-    }*/
 
     private fun loginFacebook() {
         loginManager.logInWithReadPermissions(
@@ -161,7 +134,6 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         findNavController().navigate(R.id.action_loginFragment_to_cadastroFragment)
     }
 
-
     private fun sendToHome() {
 
         findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
@@ -170,15 +142,17 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     private fun onGoogleSignInResult(result: GoogleLogInActivityContract.Result?) {
         if(result is GoogleLogInActivityContract.Result.Success){
             val token = result.googleSignInAccount.idToken
-            //token?.let{ socialLogin}
-            Toast.makeText(requireContext(), "Meu token do google e -> $token", Toast.LENGTH_LONG).show()
+            sendToHome()
+            Toast.makeText(requireContext(),"Sucesso no login", Toast.LENGTH_LONG).show()
+            //retorna token
+            //Toast.makeText(requireContext(), "Meu token do google e -> $token", Toast.LENGTH_LONG).show()
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         callbackManager.onActivityResult(requestCode, resultCode, data)
-    }
+    }*/
 
     private fun validarLogin(email: String, senha: String) {
 
