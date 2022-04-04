780686adb3a96885428f7b37c926dc380393555c
Eduardo <eduardo.coelho@javendeu.com>
2022-04-04 12:11:42 -0300

    modeling application database room

---
+++ app/src/main/java/com/example/clima/local/AppDataBase.kt
@@ -0,0 +1,18 @@
+package com.example.clima.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.clima.local.dao.AcessEvents
+import com.example.clima.local.entity.EventsEntity
+
+@Database(
+    entities = [EventsEntity::class],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class AppDataBase: RoomDatabase() {
+
+    abstract fun acessEvents(): AcessEvents
+
+}
