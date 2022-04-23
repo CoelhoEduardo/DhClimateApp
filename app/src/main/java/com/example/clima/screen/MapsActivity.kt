bf6cb2978a0cf117f9707cdca647e35adb0efa0c
Lucas Sales <lucas.gol@uol.com.br>
2022-04-23 12:54:55 -0300

    Botoes busca funcionando e trocando estado

--- app/src/main/java/com/example/clima/screen/MapsActivity.kt
+++ app/src/main/java/com/example/clima/screen/MapsActivity.kt
@@ -27,17 +27,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private val adapter = SearchAdapter()
     private val viewModel: MapViewModel by viewModels()
-
     private val recycler: RecyclerView?
         get() = findViewById(R.id.recycler_maps)
     private val loading: FrameLayout?
         get() = findViewById(R.id.loading)
 
-    val radioButton: RadioGroup?
+    val radioButton : RadioGroup?
         get() = findViewById<RadioGroup>(R.id.radio_group)
-    private val home: ImageView
+    private val home : ImageView
         get() = findViewById<ImageView>(R.id.home_button)
-    private val queimadaButton: ImageView?
+    private val queimadaButton : ImageView?
         get() = findViewById<ImageView>(R.id.queimada_button)
     val vulconButton: ImageView?
         get() = findViewById<ImageView>(R.id.vulcon_button)
@@ -58,6 +57,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapsBinding
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
@@ -66,23 +66,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        queimadaButton?.setOnClickListener {
+        queimadaButton?.setOnClickListener{
             novaRequisicao("wildfires")
         }
-        landslideButton?.setOnClickListener {
+        landslideButton?.setOnClickListener{
             novaRequisicao("landslides")
+
         }
-        iceButton?.setOnClickListener {
+        iceButton?.setOnClickListener{
             novaRequisicao("SeaLakeIce")
+
         }
-        earthquakeButton?.setOnClickListener {
+        earthquakeButton?.setOnClickListener{
             novaRequisicao("earthquakes")
+
         }
-        stormButton?.setOnClickListener {
+        stormButton?.setOnClickListener{
             novaRequisicao("severeStorms")
+
         }
-        vulconButton?.setOnClickListener {
+        vulconButton?.setOnClickListener{
             novaRequisicao("volcanoes")
+
         }
 
         home.setOnClickListener {
@@ -97,28 +102,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         initMap()
     }
 
-    //Inicializar Mapa
-    private fun initMap() {
+
+
+    private fun initMap(){
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
-
-    //region Maps
     override fun onMapReady(googleMap: GoogleMap) {
+
         map = googleMap
+
         // Add a marker in Sydney and move the camera
         //val sydney = LatLng(-34.0, 151.0)
         //map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         //map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
         //map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15f))
     }
-    //endregion
 
-    //Metodo Observe alterar dados
+
     private fun observeData() {
-        var local: LatLng = LatLng(-34.0, 148.0)
+        var local: LatLng = LatLng(-34.0,148.0)
         var title: String = "new"
         viewModel.loading.observe(this) { loading?.isVisible = it }
         viewModel.error.observe(this) {
@@ -126,46 +131,123 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
         viewModel.events.observe(this) {
             adapter.updateList(it.events)
-            //Adicionar pin ao mapa
-            for (item in it.events) {
-                local = LatLng(
-                    item.geometry.last().coordinates.last(),
-                    item.geometry.last().coordinates.first()
-                )
+            for(item in it.events){
+                local = LatLng(item.geometry.last().coordinates.last(), item.geometry.last().coordinates.first())
                 title = item.title
                 map.addMarker(MarkerOptions().position(local).title(title))
             }
         }
     }
 
-    //Requisicao da api
-    private fun novaRequisicao(string: String) {
-        viewModel.loadEventsFiltered(string, radioCheck())
-        map.clear()
+    private fun sendToHome(){
+        /*val navController = findNavController(R.id.mapsActivity)
+        navController.navigate()*/
+        finish()
     }
 
-    //filtra por evento ->desativado<-
-    /*private fun filtrarEvento(button: ImageView): String {
-        if (queimadaButton == button)
-            return "wildfires"
-        if (earthquakeButton == button)
-            return "earthquakes"
-        if (iceButton == button)
-            return "SeaLakeIce"
-        if (stormButton == button)
-            return "severeStorms"
-        if (landslideButton == button)
-            return "landslide"
-        if (vulconButton == button)
-            return "volcanoes"
-        return ""
-    }*/
+    private fun novaRequisicao(string: String){
+        viewModel.loadEventsFiltered(string,radioCheck())
+        map.clear()
+        gerenciarBotao(string)
 
-    //Verifica posicao do radio button
-    private fun radioCheck(): String {
-        if (radioOpen.isChecked)
+    }
+
+    fun queimadaOnClick(){
+        queimadaButton?.setImageResource(R.drawable.ic_queimada_black_fundo)
+        earthquakeButton?.setImageResource(R.drawable.ic_earthquake_no_back)
+        iceButton?.setImageResource(R.drawable.ic_ice_no_back)
+        stormButton?.setImageResource(R.drawable.ic_storm_no_back)
+        landslideButton?.setImageResource(R.drawable.ic_landslide_no_back)
+        vulconButton?.setImageResource(R.drawable.ic_volcano_no_back)
+    }
+    fun earthquakeOnClick(){
+        earthquakeButton?.setImageResource(R.drawable.ic_earthquake_black_fundo)
+        queimadaButton?.setImageResource(R.drawable.ic_queimada_no_back)
+        iceButton?.setImageResource(R.drawable.ic_ice_no_back)
+        stormButton?.setImageResource(R.drawable.ic_storm_no_back)
+        landslideButton?.setImageResource(R.drawable.ic_landslide_no_back)
+        vulconButton?.setImageResource(R.drawable.ic_volcano_no_back)
+    }
+    fun iceOnClick(){
+        iceButton?.setImageResource(R.drawable.ic_ice_black_fundo)
+        earthquakeButton?.setImageResource(R.drawable.ic_earthquake_no_back)
+        queimadaButton?.setImageResource(R.drawable.ic_queimada_no_back)
+        stormButton?.setImageResource(R.drawable.ic_storm_no_back)
+        landslideButton?.setImageResource(R.drawable.ic_landslide_no_back)
+        vulconButton?.setImageResource(R.drawable.ic_volcano_no_back)
+    }
+    fun stormOnClick(){
+        stormButton?.setImageResource(R.drawable.ic_storm_black_fundo)
+        earthquakeButton?.setImageResource(R.drawable.ic_earthquake_no_back)
+        iceButton?.setImageResource(R.drawable.ic_ice_no_back)
+        queimadaButton?.setImageResource(R.drawable.ic_queimada_no_back)
+        landslideButton?.setImageResource(R.drawable.ic_landslide_no_back)
+        vulconButton?.setImageResource(R.drawable.ic_volcano_no_back)
+    }
+    fun landslideOnClick(){
+        landslideButton?.setImageResource(R.drawable.ic_landslide_black_fundo)
+        earthquakeButton?.setImageResource(R.drawable.ic_earthquake_no_back)
+        iceButton?.setImageResource(R.drawable.ic_ice_no_back)
+        stormButton?.setImageResource(R.drawable.ic_storm_no_back)
+        queimadaButton?.setImageResource(R.drawable.ic_queimada_no_back)
+        vulconButton?.setImageResource(R.drawable.ic_volcano_no_back)
+    }
+    fun vulconOnClick(){
+        vulconButton?.setImageResource(R.drawable.ic_volcano_black_fundo)
+        earthquakeButton?.setImageResource(R.drawable.ic_earthquake_no_back)
+        iceButton?.setImageResource(R.drawable.ic_ice_no_back)
+        stormButton?.setImageResource(R.drawable.ic_storm_no_back)
+        landslideButton?.setImageResource(R.drawable.ic_landslide_no_back)
+        queimadaButton?.setImageResource(R.drawable.ic_queimada_no_back)
+    }
+
+    private fun gerenciarBotao(string: String) {
+        if (string == "wildfires")
+            queimadaOnClick()
+        if (string == "earthquakes")
+            earthquakeOnClick()
+        if (string == "SeaLakeIce")
+            iceOnClick()
+        if (string == "severeStorms")
+            stormOnClick()
+        if (string == "landslides")
+            landslideOnClick()
+        if (string == "volcanoes")
+            vulconOnClick()
+    }
+
+
+    private fun radioCheck(): String{
+        if(radioOpen.isChecked)
             return "open"
         else
             return "closed"
+
+
     }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.open_btn ->
+                    if (checked) {
+                        // Pirates are the best
+
+                    }
+                R.id.close_button ->
+                    if (checked) {
+                        // Ninjas rule
+
+                    }
+            }
+        }
+    }
+
+
+
+
 }
